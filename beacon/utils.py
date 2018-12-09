9c5f7e3453c1b467dc0170f2f6fae9b4a36c826d
Abeer Upadhyay <ab.esquarer@gmail.com>
2018-12-09 14:16:44 +0530

    Utility for validating DSN format
    Signed-off-by: Abeer Upadhyay <ab.esquarer@gmail.com>

--- beacon/utils.py
+++ beacon/utils.py
@@ -2,8 +2,14 @@
 # -*- coding: utf-8 -*-
 
 """Utilities for Beacon."""
+import re
 import threading
 import time
+
+try:
+    from urlparse import urlparse
+except ImportError:
+    from urllib.parse import urlparse, urlencode
 
 
 class Timer(object):
@@ -40,3 +46,108 @@ class Timer(object):
         with self.mutex:
             self.is_stopped = True
             self.timer.cancel()
+
+
+class DSN(object):
+    """
+    Validates the DSN string for the following format -
+    <scheme>://<api-key>@<beacon-server-hostname>/<repository-id>
+    """
+    ALLOWED_SCHEMES = ('http', 'https')
+
+    API_KEY_REGEX = re.compile(r'^[a-fA-F0-9]{32}$')
+    HOSTNAME_REGEX = re.compile(r'^[a-z0-9]+([-.][a-z0-9]+)*\.[a-z]{2,}(:\d+)?$')
+    REPOSITORY_REGEX = re.compile(r'^\/?(\d+)\/?$')
+
+    def __init__(self, dsn):
+
+        self.input_dsn_string = str(dsn)
+
+        if not dsn:
+            raise ValueError(self.ERROR_MESSAGE)
+
+        try:
+            # Parse the dsn as a url to form
+            # scheme:   http/https
+            # netloc:   (api-key)@(hostname)
+            # path:     /(repository_id)
+            self.parsed_dsn = urlparse(self.input_dsn_string)
+        except Exception:
+            raise ValueError(self.ERROR_MESSAGE)
+
+        self._set_scheme()
+        self._set_api_key()
+        self._set_host()
+        self._set_repository_id()
+
+    def _set_scheme(self):
+        """
+        Validates the scheme in the DSN string and sets it in `scheme`
+        attribute.
+        """
+        scheme = self.parsed_dsn.scheme
+        if self.parsed_dsn.scheme not in self.ALLOWED_SCHEMES:
+            raise ValueError(self.ERROR_MESSAGE)
+
+        self.scheme = scheme
+
+    def _set_api_key(self):
+        """
+        Validates the API key in the DSN string and sets it in `api_key`
+        attribute.
+        """
+        api_key = self.parsed_dsn.netloc.split('@', 1)[0]
+        if not api_key or not self.API_KEY_REGEX.match(api_key):
+            raise ValueError(self.ERROR_MESSAGE)
+
+        self.api_key = api_key
+
+    def _set_host(self):
+        """
+        Validates the host in the DSN string and sets it in `host`
+        attribute.
+        """
+        try:
+            host = self.parsed_dsn.netloc.split('@', 1)[1]
+        except IndexError:
+            raise ValueError(self.ERROR_MESSAGE)
+
+        if not host or not self.HOSTNAME_REGEX.match(host):
+            raise ValueError(self.ERROR_MESSAGE) 
+
+        self.host = host
+
+    def _set_repository_id(self):
+        """
+        Validates the Repository ID in the DSN string and sets it in
+        `repository_id` attribute.
+        """
+        path = self.REPOSITORY_REGEX.match(self.parsed_dsn.path)
+        if not path:
+            raise ValueError(self.ERROR_MESSAGE)
+
+        try:
+            repository_id = path.group(1)
+        except IndexError:
+            raise ValueError(self.ERROR_MESSAGE)
+
+        if not repository_id.isdigit():
+            raise ValueError(self.ERROR_MESSAGE)
+
+        self.repository_id = repository_id
+
+    def __str__(self):
+        return "{scheme}://{api_key}@{host}/{repository_id}".format(
+            scheme=self.scheme,
+            api_key=self.api_key,
+            host=self.host,
+            repository_id=self.repository_id
+        )
+
+    @property
+    def ERROR_MESSAGE(self):
+        return (
+            "Invalid DSN string - {}. The required format is "
+            "<scheme>://<api-key>@<beacon-server-hostname>/<repository-id>"
+            .format(self.input_dsn_string)
+        )
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -45,3 +45,103 @@ def test_timer_should_raise_proper_exceptions():
     with pytest.raises(Exception) as exc:
         timer.stop()
     assert str(exc.value) == "This timer has already been stopped."
+
+
+def test_dsn_validator_with_valid_dsn():
+
+    valid_dsn_list = [
+        "http://43f62c942790426c8b41220aade4a0d8@domain.com/33",
+        "http://43f62c942790426c8b41220aade4a0d8@stagin.domain.com/33",
+        "https://43f62c942790426c8b41220aade4a0d8@domain.io/33",
+        "https://43f62c942790426c8b41220aade4a0d8@domain.io:80/33",
+        "https://43f62c942790426c8b41220aade4a0d8@domain.com/33/"
+    ]
+
+    # No exceptions should be raised as all dsns are valid
+    for dsn_input_string in valid_dsn_list:
+        dsn = utils.DSN(dsn_input_string)
+        assert str(dsn) in dsn_input_string
+
+
+def test_dsn_validator_with_invalid_dsn():
+
+    invalid_dsn_list = [
+        "",
+        "foobar"
+        "43f62c942790426c8b41220aade4a0d8@domain.com/",
+        "domain.com",
+        None,
+        {},
+        0,
+        [],
+    ]
+
+    for dsn in invalid_dsn_list:
+        
+        with pytest.raises(ValueError) as exc:
+            utils.DSN(dsn)
+            assert str(exc.value) == utils.DSN.ERROR_MESSAGE
+
+
+def test_dsn_validator_with_invalid_scheme():
+
+    valid_dsn_list = [
+        "ftp://43f62c942790426c8b41220aade4a0d8@domain.com/33",
+        "amqp://43f62c942790426c8b41220aade4a0d8@domain.com/33"
+    ]
+
+    # Should raise value error as only http/https schemes are allowed
+    for dsn in valid_dsn_list:
+
+        with pytest.raises(ValueError) as exc:
+            utils.DSN(dsn)
+            assert str(exc.value) == utils.DSN.ERROR_MESSAGE
+
+
+def test_dsn_validator_with_invalid_api_key():
+
+    invalid_dsn_list = [
+        "https://43q62c942790426s8b41220aade4a0d8@domain.com/33",
+        "https://43f62c942790426c8b41220aade4@domain.com/33"
+        "https://aff7912c-e170-47cb-ba92-42011c35cbc3@domain.com/33"
+    ]
+
+    # Should raise value error as api keys are valid uuid4 hex strings
+    for dsn in invalid_dsn_list:
+
+        with pytest.raises(ValueError) as exc:
+            utils.DSN(dsn)
+            assert str(exc.value) == utils.DSN.ERROR_MESSAGE
+
+
+def test_dsn_validator_with_invalid_hostname():
+
+    invalid_dsn_list = [
+        "https://43f62c942790426c8b41220aade4a0d8@do#main.com/33",
+        "https://43f62c942790426c8b41220aade4a0d8@domain.c/33"
+        "https://43f62c942790426c8b41220aade4a0d8@domain.com:foo/33"
+    ]
+
+    # Should raise value error as provided hostnames are not valid
+    for dsn in invalid_dsn_list:
+
+        with pytest.raises(ValueError) as exc:
+            utils.DSN(dsn)
+            assert str(exc.value) == utils.DSN.ERROR_MESSAGE
+
+
+def test_dsn_validator_with_invalid_repository_id():
+
+    invalid_dsn_list = [
+        "https://43f62c942790426c8b41220aade4a0d8@domain.com/33a",
+        "https://43f62c942790426c8b41220aade4a0d8@domain.com/33/foo/bar"
+        "https://43f62c942790426c8b41220aade4a0d8@domain.com/"
+    ]
+
+    # Should raise value error as provided repository_ids are not valid
+    for dsn in invalid_dsn_list:
+
+        with pytest.raises(ValueError) as exc:
+            utils.DSN(dsn)
+            assert str(exc.value) == utils.DSN.ERROR_MESSAGE
+
